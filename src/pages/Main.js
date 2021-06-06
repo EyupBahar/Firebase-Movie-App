d0b6f9ab8974e56ab85da410e19a3d431a0e0ce9
7204-OMER <Eyupbhr24@gmail.com>
2021-06-06 13:15:49 +0300

    add movies API

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 const Main = () => {
+  const [movies, setMovies] = useState([]);
+  const [searcTerm, setSearcTerm] = useState("");
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((res) => setMovies(res));
+  };
+
   return <div>MAIN</div>;
 };
 
